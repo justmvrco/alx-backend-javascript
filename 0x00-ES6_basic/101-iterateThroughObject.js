20030debcd3905c3a1aadd0ebb2fdc1c17ea358e
justmvrco <moreirapiliki@gmail.com>
2022-04-04 19:56:57 +0000

    Node js task 101

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,5 +1,13 @@
 export default function iterateThroughObject(reportWithIterator) {
-  if (!reportWithIterator) {
-    console.log('Placeholder');
+  let employees = '';
+
+  for (const [i, value] of reportWithIterator.entries()) {
+    if (i !== reportWithIterator.length - 1) {
+      employees += `${value} | `;
+    } else {
+      employees += value;
+    }
   }
+
+  return employees;
 }
